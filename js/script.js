c5d8a63d6ec852713f2addc1ee263617d91db989
Junior DeLonge <147082536+JuniorDeLonge@users.noreply.github.com>
2023-11-15 12:50:09 -0300

    v2.7

--- js/script.js
+++ js/script.js
@@ -1,46 +1,63 @@
+// Constantes para a API e URLs
+const API_KEY = '3adf342557400a33545abde1b7a7bca9';
+const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5/weather';
+const FORECAST_API_URL = 'https://api.openweathermap.org/data/2.5/forecast';
+const GEO_API_URL = 'https://api.openweathermap.org/geo/1.0/reverse';
+
+// Condições climáticas
+const weatherConditions = {
+    'Clear': "url('imagens/claro.jpg')",
+    'Rain': "url('imagens/chuva.jpg')",
+    'Clouds': "url('imagens/nuvens.jpg')",
+    'Snow': "url('imagens/neve.jpg')",
+    'Drizzle': "url('imagens/chuvisco.jpg')",
+    'Thunderstorm': "url('imagens/tempestade.jpg')",
+    'Fog': "url('imagens/neblina.jpg')",
+    'Mist': "url('imagens/nevoa.jpg')",
+    'Haze': "url('imagens/bruma.jpg')",
+    'Smoke': "url('imagens/fumaca.jpg')",
+    'Dust': "url('imagens/poeira.jpg')",
+    'Sand': "url('imagens/areia.jpg')",
+    'Ash': "url('imagens/cinzas.jpg')",
+    'Squall': "url('imagens/rajada.jpg')",
+    'Tornado': "url('imagens/tornado.jpg')"
+};
+
 // Função para alterar o plano de fundo com base no clima
 function changeBackground(clima) {
     const body = document.body;
-    const weatherImages = {
-        'Clear': "url('imagens/claro.jpg')",
-        'Rain': "url('imagens/chuva.jpg')",
-        'Clouds': "url('imagens/nuvens.jpg')",
-        'Snow': "url('imagens/neve.jpg')",
-        'Drizzle': "url('imagens/chuvisco.jpg')",
-        'Thunderstorm': "url('imagens/tempestade.jpg')",
-        'Fog': "url('imagens/neblina.jpg')",
-        'Mist': "url('imagens/nevoa.jpg')",
-        'Haze': "url('imagens/bruma.jpg')",
-        'Smoke': "url('imagens/fumaca.jpg')",
-        'Dust': "url('imagens/poeira.jpg')",
-        'Sand': "url('imagens/areia.jpg')",
-        'Ash': "url('imagens/cinzas.jpg')",
-        'Squall': "url('imagens/rajada.jpg')",
-        'Tornado': "url('imagens/tornado.jpg')"
-    };
-    body.style.backgroundImage = weatherImages[clima] || '';
+    body.style.backgroundImage = weatherConditions[clima] || '';
 }
 
+// Função assíncrona para buscar dados climáticos
 async function fetchWeatherData(city) {
     const trimmedCity = city.trim();
 
     document.getElementById('loading').classList.remove('hidden');
     try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(trimmedCity)}&appid=3adf342557400a33545abde1b7a7bca9&units=metric&lang=pt_br`);
+        const response = await fetch(`${WEATHER_API_URL}?q=${encodeURI(trimmedCity)}&appid=${API_KEY}&units=metric&lang=pt_br`);
         if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
+            const errorMessage = await response.json();
+            throw new Error(`HTTP error! status: ${response.status}, message: ${errorMessage.message}`);
         }
         const data = await response.json();
         document.getElementById('loading').classList.add('hidden');
         return data;
     } catch (error) {
         document.getElementById('loading').classList.add('hidden');
-        alert("Não foi possível encontrar a cidade. Verifique o nome e tente novamente.");
+        displayError("Não foi possível encontrar a cidade. Verifique o nome e tente novamente.");
+        throw error;
     }
 }
 
+// Função para exibir mensagens de erro
+function displayError(message) {
+    const errorMessage = document.getElementById('error-message');
+    errorMessage.innerText = message;
+    errorMessage.style.display = 'block';
+}
 
-// Função para exibir os dados do tempo na página
+// Função para exibir dados climáticos na página
 function displayWeatherData(data) {
     const weatherConditions = {
         'Clear': 'Claro',
@@ -59,6 +76,7 @@ function displayWeatherData(data) {
         'Squall': 'Rajada',
         'Tornado': 'Tornado'
     };
+
     document.getElementById('city-name').textContent = 'Tempo em: ' + data.name;
     document.getElementById('temperature').textContent = 'Temperatura atual: ' + data.main.temp + '°C';
     document.getElementById('condition').textContent = 'Condição do tempo: ' + (weatherConditions[data.weather[0].main] || data.weather[0].main);
@@ -68,17 +86,18 @@ function displayWeatherData(data) {
     document.getElementById('pressure').textContent = 'Pressão atmosférica: ' + data.main.pressure + ' mb';
     document.getElementById('precipitation').textContent = 'Precipitação: ' + (data.rain ? data.rain['1h'] : 0) + ' mm';
     document.getElementById('visibility').textContent = 'Visibilidade: ' + (data.visibility ? data.visibility / 1000 : 'N/A') + ' km';
+
     if (data.alerts) {
-        console.log(data.alerts);  // Adicione esta linha para verificar os alertas
+        console.log(data.alerts);
         document.getElementById('alert').textContent = 'Alerta: ' + data.alerts[0].description;
     } else {
         document.getElementById('alert').textContent = 'Sem alertas no momento.';
     }
-    changeBackground(data.weather[0].main); // Adicione esta linha
+
+    changeBackground(data.weather[0].main);
 }
 
-
-// Função para buscar e exibir os dados do tempo
+// Função para buscar dados climáticos e exibir na página
 async function getWeather(city) {
     try {
         const data = await fetchWeatherData(city);
@@ -88,6 +107,7 @@ async function getWeather(city) {
     }
 }
 
+// Adiciona um ouvinte de evento para a tecla Enter no campo de entrada da cidade
 document.getElementById('city-input').addEventListener('keyup', function (event) {
     if (event.keyCode === 13) {
         event.preventDefault();
@@ -97,10 +117,9 @@ document.getElementById('city-input').addEventListener('keyup', function (event)
     }
 });
 
-
-// Função para buscar o nome da cidade com base na latitude e longitude
+// Função para obter o nome da cidade com base na latitude e longitude
 async function getCityName(lat, lon) {
-    const response = await fetch(`https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=3adf342557400a33545abde1b7a7bca9`);
+    const response = await fetch(`${GEO_API_URL}?lat=${lat}&lon=${lon}&limit=1&appid=${API_KEY}`);
     if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
     }
@@ -108,7 +127,7 @@ async function getCityName(lat, lon) {
     return data[0].name;
 }
 
-// Função para buscar a localização atual do usuário
+// Função para obter a localização atual do usuário
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(async position => {
@@ -125,7 +144,7 @@ function getLocation() {
     }
 }
 
-// Adicionando event listeners para os botões
+// Adiciona ouvintes de eventos para os botões
 document.getElementById('location-button').addEventListener('click', getLocation);
 document.getElementById('search-button').addEventListener('click', function (event) {
     event.preventDefault();
@@ -137,12 +156,14 @@ document.getElementById('search-button').addEventListener('click', function (eve
 // Função para buscar e exibir a previsão do tempo
 async function getForecast(city) {
     try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${encodeURI(city)}&appid=3adf342557400a33545abde1b7a7bca9&units=metric&lang=pt_br`);
+        const response = await fetch(`${FORECAST_API_URL}?q=${encodeURI(city)}&appid=${API_KEY}&units=metric&lang=pt_br`);
         if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
+            const errorMessage = await response.json();
+            throw new Error(`HTTP error! status: ${response.status}, message: ${errorMessage.message}`);
         }
         const data = await response.json();
         const daysOfWeek = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
+
         for (let i = 0; i < data.list.length; i += 8) {
             const date = new Date(data.list[i].dt_txt);
             const dayOfWeek = daysOfWeek[date.getDay()];
@@ -150,29 +171,14 @@ async function getForecast(city) {
             forecastElement.textContent = `${dayOfWeek} | Mín: ${data.list[i].main.temp_min}°C | Máx: ${data.list[i].main.temp_max}°C | ${data.list[i].weather[0].description.charAt(0).toUpperCase() + data.list[i].weather[0].description.slice(1)}.`;
             forecastElement.style.display = 'block';
         }
-        // Adicione o nome da cidade à mensagem de previsão
+
         document.getElementById('forecast').textContent = 'Previsão para os próximos dias em ' + data.city.name;
     } catch (error) {
         console.error('Erro:', error);
     }
 }
 
-// Esconda inicialmente a seção de previsão
+// Esconde inicialmente a seção de previsão
 for (let i = 1; i <= 5; i++) {
     document.getElementById(`day${i}`).style.display = 'none';
 }
-
-// Adicione um ouvinte de evento ao botão de pesquisa
-document.getElementById('search-button').addEventListener('click', function () {
-    var city = document.getElementById('city-input').value;
-    if (city) {
-        // Se uma cidade foi pesquisada, busque a previsão do tempo
-        getForecast(city);
-    } else {
-        // Se nenhuma cidade foi pesquisada, esconda a seção de previsão
-        for (let i = 1; i <= 5; i++) {
-            document.getElementById(`day${i}`).style.display = 'none';
-        }
-    }
-});
-
